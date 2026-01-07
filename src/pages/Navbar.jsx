6dd08be23bcfe0367b7aeee7d2a5f5419401163c
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-07 23:48:39 +0530

    style navlink

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -7,8 +7,18 @@ const Navbar = () => {
       <h1>navbar</h1>
       <div className='flex gap-4 text-black p-2'>
         
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <NavLink to="/" style={({ isActive }) => ({
+                    color: isActive ? "red" : "black",
+                    fontWeight: isActive ? "bold" : "normal",
+                    textDecoration: "none",
+                    marginRight: "15px",
+                })}>Home</NavLink>
+        <NavLink to="/about" style={({ isActive }) => ({
+                    color: isActive ? "red" : "black",
+                    fontWeight: isActive ? "bold" : "normal",
+                    textDecoration: "none",
+                    marginRight: "15px",
+                })}>About</NavLink>
       </div>
     </div>
   )
