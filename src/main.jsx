49023e7203b31914b24bedfbda6a14f9a2e04ba9
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-13 23:19:54 +0530

    wrapped app in theme context

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import UserContext from './context/UserContext.jsx'
+import ThemeContext from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <UserContext>
-        <App />
+       <ThemeContext>
+         <App />
+       </ThemeContext>
       </UserContext>
     </BrowserRouter>
   </StrictMode>
