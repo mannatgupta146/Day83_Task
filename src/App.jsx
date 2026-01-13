55b3cfc63b8adcf6c17734801803c7396f8183bc
mannatgupta146 <mannatgupta146@gmail.com>
2026-01-13 23:45:55 +0530

    cleaning for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useContext, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import About from './pages/About'
 import Navbar from './pages/Navbar'
-import Section from './pages/Section'
 
 const App = () => {
   const [theme, setTheme] = useState('light')
@@ -13,9 +11,6 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme} />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/about/:id' element={<About />} />
-        <Route path='/section' element={<Section />} />
       </Routes>
     </div>
   )
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,59 +1,9 @@
-import React, { useContext, useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import { UserDataContext } from '../context/UserContext'
+import React from 'react'
 
-const Navbar = ({ theme, setTheme }) => {
-  const [inputValue, setInputValue] = useState('')
-  const data = useContext(UserDataContext)
-
-  const navStyle = ({ isActive }) =>
-    `px-3 py-1 rounded transition-all duration-200
-     ${isActive 
-        ? 'text-blue-600 font-semibold border-b-2 border-blue-600' 
-        : 'text-gray-700 hover:text-blue-500'
-     }`
-
+const Navbar = () => {
   return (
-    <div className="flex justify-center items-center gap-6 p-3 shadow">
-      <h1 className="text-sm">
-        theme is <span className="font-semibold">{theme}</span> {data}
-      </h1>
-
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          setTheme(inputValue)
-          setInputValue('')
-        }}
-        className="flex gap-2"
-      >
-        <input
-          type="text"
-          placeholder="enter color"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          className="border px-2 py-1 rounded"
-        />
-
-        <button
-          type="submit"
-          className="px-3 py-1 bg-black text-white rounded"
-        >
-          Submit
-        </button>
-      </form>
-
-      <NavLink to="/" className={navStyle}>
-        Home
-      </NavLink>
-
-      <NavLink to="/about" className={navStyle}>
-        About
-      </NavLink>
-
-      <NavLink to="/section" className={navStyle}>
-        Section
-      </NavLink>
+    <div>
+      
     </div>
   )
 }
